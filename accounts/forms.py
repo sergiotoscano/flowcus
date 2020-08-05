d04b4904df799865f769722f229e98bc013e4250
Sergio Toscano <stoscanot@gmail.com>
2020-08-05 10:51:54 -0700

    navbar + frontpage for visitors design  done

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,16 +6,8 @@ from django.contrib.auth.models import User
 
 
 
-#class SignupForm(UserCreationForm):
-#   fields = ('username', 'email', 'password1', 'password2')
-    #those fields are default for UserCreationForm - https://docs.djangoproject.com/en/1.8/_modules/django/contrib/auth/forms/
-    #below we customize those field names to be more user friendly
-
-
-
-
 class SignupForm(UserCreationForm):
-    email = forms.EmailField(required=False)
+    email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
@@ -26,39 +18,8 @@ class SignupForm(UserCreationForm):
         if commit:
             user.save()
         return user
-#            user = authenticate(username=self.cleaned_data['username'],
-#                                password=self.cleaned_data['password1'])
-#            if user is not None:
-#                return HttpResponseRedirect("home.html")
-#            else:
-#                console.log('not authenticated')
-#            login(request, user)
-#            
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['username'].label = 'Account Name'
-        self.fields['email'].label = 'E-mail Address'
-
-
-
-
-
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            password = self.request.POST.get('password', None)
-            authenticated = authenticate(
-                username=user.username,
-                password=password
-            )
-            if authenticated:
-                login(request, authenticated)
-                return redirect("/")
-    else:
-        form = UserCreationForm()
-    return render(request, 'register.html', {
-        'form': form
-    })
+        self.fields['username'].label = 'Username'
+        self.fields['email'].label = 'Your best e-mail'
--- accounts/urls.py
+++ accounts/urls.py
@@ -16,14 +16,11 @@ urlpatterns = [
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset_form.html',
                                                                 email_template_name='accounts/password_reset_email.html',
                                                                 subject_template_name='accounts/password_reset_subject.txt',
-     #                                                           success_url='accounts/login',
                                                                 from_email='noreply@flowcus.com',
                                                                 ), name='password_reset'),
     path('password_reset/done/', views.PasswordResetDoneView.as_view()),
     re_path('reset/(?P<uidb64>[-\w]+)/(?P<token>[-\w]+)/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html')),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html')),
     
-#    http://127.0.0.1:8000/accounts/reset/MzE/5ir-73d1323d6c4de43a4374/
- #   {{ protocol}}://{{ domain }}{% url 'password_reset_confirm' uidb64=uid token=token %}
 ]
 
